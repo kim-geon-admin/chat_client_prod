1ff01655bfb79167625c155f4cd802e1a37381b8
kg <nayaguny@gmail.com>
2023-06-10 08:37:26 +0900

    20230610

--- src/index.js
+++ src/index.js
@@ -5,16 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-   //  <React.StrictMode>
-           <App />
-  //   </React.StrictMode>,
-    , document.getElementById('root')
-   );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
 
-
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
   ],
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'docs'),
+    //  directory: path.resolve(__dirname, 'docs'),
     },
     port: 3001,
   },
